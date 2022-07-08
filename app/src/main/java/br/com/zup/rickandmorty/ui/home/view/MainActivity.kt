e04084f84ff8c5fcc5f221746d9208ec4e2befb0
CatarineRodrigues <catarine_pacheco@hotmail.com>
2022-07-08 11:53:41 -0300

    Adicionar dependencias android manifest

--- app/src/main/java/br/com/zup/rickandmorty/ui/home/view/MainActivity.kt
+++ app/src/main/java/br/com/zup/rickandmorty/ui/home/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package br.com.zup.rickandmorty
+package br.com.zup.rickandmorty.ui.home.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.zup.rickandmorty.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/br/com/zup/rickandmorty/ui/splash/SplashActivity.kt
@@ -0,0 +1,12 @@
+package br.com.zup.rickandmorty.ui.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.zup.rickandmorty.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
