bb44cb05728c12577f92d2c5ddbd4c595f40b69e
CatarineRodrigues <catarine_pacheco@hotmail.com>
2022-07-09 19:55:41 -0300

    Implementar funçcoes no CharacterRepository

--- app/src/main/java/br/com/zup/rickandmorty/domain/repository/CharacterRepository.kt
+++ app/src/main/java/br/com/zup/rickandmorty/domain/repository/CharacterRepository.kt
@@ -1,4 +1,17 @@
 package br.com.zup.rickandmorty.domain.repository
 
-class CharacterRepository {
+import br.com.zup.rickandmorty.data.datasource.local.dao.CharacterDAO
+import br.com.zup.rickandmorty.data.datasource.remote.RetrofitService
+import br.com.zup.rickandmorty.data.datasource.remote.model.CharacterResponse
+import br.com.zup.rickandmorty.data.datasource.remote.model.CharacterResult
+
+class CharacterRepository(private val characterDAO: CharacterDAO) {
+
+    fun getAllCharacters(): List<CharacterResult> = characterDAO.getAllCharacters()
+
+    suspend fun insertAllCharactersDB(characterList: List<CharacterResult>) =
+        characterDAO.insertAllCharactersDB(characterList)
+
+    suspend fun geAllMoviesNetwork(): CharacterResponse =
+        RetrofitService.apiService.getAllCharactersNetwork()
 }
