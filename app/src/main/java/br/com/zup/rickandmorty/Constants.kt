26935dca73ad792890528735a73ed23f760a696d
CatarineRodrigues <catarine_pacheco@hotmail.com>
2022-07-08 22:19:32 -0300

    Implementar métodos adapter

---
+++ app/src/main/java/br/com/zup/rickandmorty/Constants.kt
@@ -0,0 +1,3 @@
+package br.com.zup.rickandmorty
+
+const val URL_BASE_IMAGE = "https://rickandmortyapi.com/api/character/avatar/"
--- app/src/main/java/br/com/zup/rickandmorty/ui/characterlist/view/adapter/CharacterAdapter.kt
+++ app/src/main/java/br/com/zup/rickandmorty/ui/characterlist/view/adapter/CharacterAdapter.kt
@@ -3,15 +3,18 @@ package br.com.zup.rickandmorty.ui.characterlist.view.adapter
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import br.com.zup.rickandmorty.URL_BASE_IMAGE
 import br.com.zup.rickandmorty.data.datasource.remote.model.CharacterResult
 import br.com.zup.rickandmorty.databinding.CharacterItemBinding
+import com.squareup.picasso.Picasso
 
 class CharacterAdapter(
-    private var listCharacter: MutableList<CharacterResult>,
+    private var characterList: MutableList<CharacterResult>,
 ) : RecyclerView.Adapter<CharacterAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val binding = CharacterItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val binding =
+            CharacterItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return ViewHolder(binding)
     }
 
@@ -19,9 +22,20 @@ class CharacterAdapter(
         TODO("Not yet implemented")
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+    override fun getItemCount() = characterList.size
+
+    fun updateCharacterList(newList: MutableList<CharacterResult>) {
+        characterList = newList
+        notifyDataSetChanged()
     }
 
-    class ViewHolder(val binding: CharacterItemBinding) : RecyclerView.ViewHolder(binding.root)
+    class ViewHolder(val binding: CharacterItemBinding) : RecyclerView.ViewHolder(binding.root) {
+
+        fun showCharacterInfo(characterResult: CharacterResult) {
+            binding.tvCharacter.text = characterResult.name
+            Picasso.get().load(characterResult.image)
+                .into(binding.imageCharacter)
+        }
+    }
+
 }
