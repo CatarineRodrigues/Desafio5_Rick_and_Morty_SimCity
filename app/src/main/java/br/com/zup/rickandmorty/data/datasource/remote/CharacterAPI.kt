451c0643abecf9683a5fcacdb585a19970edce72
CatarineRodrigues <catarine_pacheco@hotmail.com>
2022-07-10 12:32:36 -0300

    Testar exibir mais personagens

--- app/src/main/java/br/com/zup/rickandmorty/data/datasource/remote/CharacterAPI.kt
+++ app/src/main/java/br/com/zup/rickandmorty/data/datasource/remote/CharacterAPI.kt
@@ -1,11 +1,15 @@
 package br.com.zup.rickandmorty.data.datasource.remote
 
+
 import br.com.zup.rickandmorty.data.datasource.remote.model.CharacterResponse
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface CharacterAPI {
 
     @GET("character")
     suspend fun getAllCharactersNetwork(
+        @Query("page")
+        pages: Int = 1
     ): CharacterResponse
 }
